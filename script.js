ce4beea5cb888e7e91a4a822a6dcb57d4798766b
jissac <jissac@users.noreply.github.com>
2014-12-26 13:44:12 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,40 @@
+$(document).ready(function() { 
+	$('#orb').click(function() {
+		if ($(this).hasClass('sun')) {
+			$(this).removeClass('sun').addClass('moon');
+		}
+		else{
+			$(this).removeClass('moon').addClass('sun');
+		}
+		
+		if ($('#sky').hasClass('day')) {
+			$('#sky').removeClass('day').addClass('night');
+		}
+		else{
+			$('#sky').removeClass('night').addClass('day');
+		}
+		
+		if($('#moonspot1').hasClass('visible')) {
+			$('#moonspot1').removeClass('visible');
+		}
+		else {
+			$('#moonspot1').addClass('visible');
+		}
+		
+		if($('#moonspot2').hasClass('visible')) {
+			$('#moonspot2').removeClass('visible');
+		}
+		else {
+			$('#moonspot2').addClass('visible');
+		}
+		
+		if($('#moonspot3').hasClass('visible')) {
+			$('#moonspot3').removeClass('visible');
+		}
+		else {
+			$('#moonspot3').addClass('visible');
+		}
+		
+	});
+	
+});
